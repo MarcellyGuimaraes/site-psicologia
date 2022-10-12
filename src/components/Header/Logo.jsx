e16c68502f33289f9162f1bec3b43e1dbac2b0aa
IsisG13 <isissouzaguimaraes@gmail.com>
2022-10-11 22:01:56 -0300

    descobri o bug no css (agora tá mais arrumado, mas não tá pronto)

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import logo from '../../assets/logo.jpg'
 import './styles.css'
@@ -6,9 +7,11 @@ const Logo = () => {
   return (
     <div className="col-lg-3 col-md-4 col-sm-12">
       <img src={logo} alt="" />
-      <a data-toggle="collapse" data-target="#menu" href="#menu">
-        <i className="fas d-block d-md-none small-menu fa-bars"></i>
-      </a>
+
+      <FontAwesomeIcon
+        icon="fa-solid fa-bars"
+        className="d-block d-md-none small-menu"
+      />
     </div>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,39 +6,39 @@ const Header = () => {
   return (
     <header id="menu-jk" className="scroll_to_fixed_fixed">
       <nav className="">
-        {/* <div className="container"> */}
-        <div className="row nav-ro">
-          <Logo />
-          <div
-            id="menu"
-            className="col-lg-7 col-md-8 d-none d-md-block no-padding"
-          >
-            <ul>
-              <li className="item">
-                <a href="index.html">Home</a>
-              </li>
-              <li className="item">
-                <a href="about_us.html">About Us</a>
-              </li>
-              <li className="item">
-                <a href="services.html">Services</a>
-              </li>
-              <li className="item">
-                <a href="blog.html">Blog</a>
-              </li>
-              <li className="item">
-                <a href="gallery.html">Gallery</a>
-              </li>
-              <li className="item">
-                <a href="contact_us.html">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-sm-2 d-none d-lg-block">
-            <button className="btn btn-success">Book an Appointment</button>
+        <div className="container">
+          <div className="row nav-ro">
+            <Logo />
+            <div
+              id="menu"
+              className="col-lg-7 col-md-8 d-none d-md-block no-padding"
+            >
+              <ul>
+                <li className="item_li">
+                  <a href="index.html">Home</a>
+                </li>
+                <li className="item_li">
+                  <a href="about_us.html">About Us</a>
+                </li>
+                <li className="item_li">
+                  <a href="services.html">Services</a>
+                </li>
+                <li className="item_li">
+                  <a href="blog.html">Blog</a>
+                </li>
+                <li className="item_li">
+                  <a href="gallery.html">Gallery</a>
+                </li>
+                <li className="item_li">
+                  <a href="contact_us.html">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-sm-2 d-none d-lg-block">
+              <button className="btn btn-success">Book an Appointment</button>
+            </div>
           </div>
         </div>
-        {/* </div> */}
       </nav>
     </header>
   )
