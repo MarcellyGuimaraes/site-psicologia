c2da4d5e41363cb71bcc69a01f23dafcac84b972
IsisG13 <isissouzaguimaraes@gmail.com>
2022-10-21 19:52:47 -0300

    arrumando algumas coisas no 'doctor message'

--- src/components/DoctorMessage/index.jsx
+++ src/components/DoctorMessage/index.jsx
@@ -12,18 +12,19 @@ const DoctorMessage = () => {
               <img src={doctt} alt="" />
             </div>
             <div className="col-md-6 col-sm-12 doc-det">
-              <h2>Hello, I’m Doctor Sanjay</h2>
-              <span>Expert Clinical Psychologist in Manhattan</span>
+              <h2>Olá, eu sou o Doutor Edilson Soares</h2>
+              <span>Psicólogo Clínico Especialista em Manhattan</span>
 
               <p>
                 Lorem Ipsum is simply text of the Lorem Ipsum is simply my text
                 of the printing and Ipsum is simply text of the Ipsum is simply
                 text of thetypesetting Ipsum is simply text of the stry simply
                 dummy text of the printing and typesetting industry. Lorem Ipsum
-                is simply text of the Lorem Ipsum is simply my text of{' '}
+                is simply text of the Lorem Ipsum is simply my text of (APENAS
+                UM TEXTO DE TESTE)
               </p>
 
-              <h4>Call me on : +123 98 8887</h4>
+              <h4>Whatsapp: +55 75 9 9123-4567</h4>
             </div>
           </div>
         </div>
