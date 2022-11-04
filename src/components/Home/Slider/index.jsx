d9d14f302a27e2ced3fd0021f3cc479b64069beb
IsisG13 <isissouzaguimaraes@gmail.com>
2022-11-04 19:52:39 -0300

    testt slider

--- src/components/Home/Slider/index.jsx
+++ src/components/Home/Slider/index.jsx
@@ -2,14 +2,32 @@ import React from 'react'
 import { slider } from '../../../assets/api'
 import './styles.css'
 
-import 'owl.carousel/dist/assets/owl.carousel.css'
-import 'owl.carousel/dist/assets/owl.theme.default.css'
-import ReactOwlCarousel from 'react-owl-carousel'
+import TinySlider from 'tiny-slider-react'
+import 'tiny-slider/dist/tiny-slider.css'
+// import 'owl.carousel/dist/assets/owl.carousel.css'
+// import 'owl.carousel/dist/assets/owl.theme.default.css'
+// import ReactOwlCarousel from 'react-owl-carousel'
 
 const Slider = () => {
+  const settings = {
+    container: '#mouse-drag2',
+    items: 3,
+    mouseDrag: true,
+    slideBy: 'page',
+    swipeAngle: false,
+    speed: 400,
+  }
+
   return (
     <div className="slider">
-      <ReactOwlCarousel items={1} margin={8} autoplay={true} loop>
+      <TinySlider settings={settings}>
+        {slider.map((i) => (
+          <div key={i.id} style={{ position: 'relative' }}>
+            <img className={`tns-lazy-img`} src={i.img} alt="" />
+          </div>
+        ))}
+      </TinySlider>
+      {/* <ReactOwlCarousel items={1} margin={8} autoplay={true} loop>
         {slider.map((i) => (
           <div key={i.id} className="item">
             <div className="slider-img">
@@ -30,7 +48,7 @@ const Slider = () => {
             </div>
           </div>
         ))}
-      </ReactOwlCarousel>
+      </ReactOwlCarousel> */}
     </div>
   )
 }
